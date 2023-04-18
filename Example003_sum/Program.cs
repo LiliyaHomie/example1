1307733c115c127da887fd6384c4b6a6c850f257
Liliya <liliyalik@gmail.com>
2023-04-18 21:11:20 +0300

    random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
--- Example004_dif/Program.cs
+++ Example004_dif/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
