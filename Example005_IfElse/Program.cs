5b676c15bca388c71b5de929491b29c957ba9285
Liliya <liliyalik@gmail.com>
2023-04-18 21:23:16 +0300

    5

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лиля")
+{
+    Console.WriteLine("WOW, это же Лиля");
+}
+else
+{
+    Console.Write("Hi ");
+    Console.WriteLine(username);
+}
