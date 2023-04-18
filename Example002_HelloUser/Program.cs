e8ee0a77197f616ecc66547dea051a51d73f20ae
Liliya <liliyalik@gmail.com>
2023-04-18 21:34:27 +0300

    6

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
---
+++ Example006_base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
